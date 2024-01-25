edd1d1e59bde83acf1cae73ecd40c173499655f7
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2024-01-24 22:00:15 -0600

    ?

--- scripts/lexical-analysis.js
+++ scripts/lexical-analysis.js
@@ -1,35 +1,42 @@
 const TogoDictionary = [
-    { tipo: "Palabra reservada", nombre: "fn", regex: /^fn\b/ },
-    { tipo: "Palabra reservada", nombre: "str", regex: /^str\b/ },
-    { tipo : "Palabra reservada", nombre: "void", regex: /^void\b/ },
-    { tipo : "Palabra reservada", nombre: "bool", regex: /^bool\b/ },
-    { tipo : "Palabra reservada", nombre: "num", regex: /^num\b/ },
-    { tipo : "Palabra reservada", nombre: "if", regex: /^if\b/ },
-    { tipo : "Palabra reservada", nombre: "for", regex: /^for\b/ },
-    { tipo: "Variable", nombre: "Cadena de Caracteres", regex: /^"([^"]*)"/ },
-    { tipo: "Variables", nombre: "Booleano", regex: /^(true|false)\b/ },
-    { tipo: "Variable", nombre: "Números", regex: /^\d+(\.\d+)?/ },
-    { tipo: "Variables", nombre: "Variable", regex: /^[a-zA-Z_][a-zA-Z0-9_]*/ },
-    { tipo: "Simbolo", nombre: "Paréntesis Apertura", regex: /^\(/ },
-    { tipo: "Simbolo", nombre: "Paréntesis Cierre", regex: /^\)/ },
-    { tipo: "Simbolo", nombre: "Llaves Apertura", regex: /^\{/ },
-    { tipo: "Simbolo", nombre: "Llaves Cierre", regex: /^\}/ },
-    { tipo: "Simbolos", nombre: "Dos puntos", regex: /^:/ },
-    { tipo: "Simbolos", nombre: "Coma", regex: /^,/ },
-    { tipo: "Simbolos", nombre: "Punto y coma", regex: /^;/ },
-    { tipo: "Símbolos", nombre: "Triple igual", regex: /^\=\=\=/ },
-    { tipo: "Símbolos", nombre: "Doble igual", regex: /^\=\=/ },
-    { tipo: "Símbolos", nombre: "Igual", regex: /^=/ },
-    { tipo: "Símbolos", nombre: "Doble distinto de", regex: /^\!\=\=/ },
-    { tipo: "Símbolos", nombre: "Distinto de", regex: /^\!\=/ },
-    { tipo: "Símbolos", nombre: "Menor igual", regex: /^\<\=/ },
-    { tipo: "Símbolos", nombre: "Mayor igual", regex: /^\>\=/ },
-    { tipo: "Operador", nombre: "Menor que", regex: /^</ },
-    { tipo: "Símbolos", nombre: "Mayor que", regex: /^>/ },
-    { tipo: "Operador", nombre: "Incremento", regex: /^\+\+/ },
-    { tipo: "Operador", nombre: "Decremento", regex: /^--/ },
+    { tipo: "Palabra reservada", regex: /^fn\b/ },
+    { tipo: "Palabra reservada", regex: /^str\b/ },
+    { tipo: "Palabra reservada", regex: /^void\b/ },
+    { tipo: "Palabra reservada", regex: /^bool\b/ },
+    { tipo: "Palabra reservada", regex: /^num\b/ },
+    { tipo: "Palabra reservada", regex: /^if\b/ },
+    { tipo: "Palabra reservada", regex: /^for\b/ },
+    { tipo: "Palabra reservada", regex: /^(true|false)\b/ },
+    { tipo: "Identificador", regex: /^"([^"]*)"/ },
+    { tipo: "Identificador", regex: /^\d+(\.\d+)?/ },
+    { tipo: "Identificador", regex: /^[a-zA-Z_][a-zA-Z0-9_]*/ },
+    { tipo: "Operador", regex: /^\+\+/ },
+    { tipo: "Operador", regex: /^--/ },
+    { tipo: "Paréntesis", regex: /^\(/ },
+    { tipo: "Paréntesis", regex: /^\)/ },
+    { tipo: "Llaves", regex: /^\{/ },
+    { tipo: "Llaves", regex: /^\}/ },
+    { tipo: "Simbolos", regex: /^:/ },
+    { tipo: "Simbolos", regex: /^,/ },
+    { tipo: "Simbolos", regex: /^;/ },
+    { tipo: "Simbolos", regex: /^\=\=\=/ },
+    { tipo: "Simbolos", regex: /^\=\=/ },
+    { tipo: "Simbolos", regex: /^=/ },
+    { tipo: "Simbolos", regex: /^\!\=\=/ },
+    { tipo: "Simbolos", regex: /^\!\=/ },
+    { tipo: "Simbolos", regex: /^\<\=/ },
+    { tipo: "Simbolos", regex: /^\>\=/ },
+    { tipo: "Simbolos", regex: /^</ },
+    { tipo: "Simbolos", regex: /^>/ },
 ]
 
+const palabrasReservadasList = new Set();
+const identificadoresList = new Set();
+const operadorList = new Set();
+const parentesisList = new Set();
+const llavesList = new Set();
+const simbolosList = new Set();
+const unknownList = new Set();
 
 class Lexer {
     constructor(input) {
@@ -44,17 +51,17 @@ class Lexer {
             return null;
         }
 
-        for (const { tipo, nombre, regex } of this.dictionary) {
+        for (const { tipo, regex } of this.dictionary) {
             const match = this.input.substr(this.pos).match(regex);
-
             if (match && match.index === 0) {
-                this.pos += match[0].length;
-                return { type: tipo, value: match[0], nombre: nombre };
+                const value = match[0];
+                this.addToTokenList(tipo, value);
+                this.pos += value.length;
+                return { type: tipo, value };
             }
         }
 
         let char = this.input[this.pos];
-
 
         if (/[a-zA-Z0-9_]/.test(char)) {
             let word = '';
@@ -63,10 +70,12 @@ class Lexer {
                 this.pos++;
                 char = this.input[this.pos];
             }
+            this.addToTokenList('Identificador', word);
             return { type: 'Identificador', value: word };
         }
 
         this.pos++;
+        this.addToTokenList('UNKNOWN', char);
         return { type: 'UNKNOWN', value: char };
     }
 
@@ -75,46 +84,119 @@ class Lexer {
             this.pos++;
         }
     }
+
+    addToTokenList(tipo, value) {
+        switch (tipo) {
+            case 'Palabra reservada':
+                palabrasReservadasList.add(value);
+                break;
+            case 'Identificador':
+                identificadoresList.add(value);
+                break;
+            case 'Operador':
+                operadorList.add(value);
+                break;
+            case 'Paréntesis':
+                parentesisList.add(value);
+                break;
+            case 'Llaves':
+                llavesList.add(value);
+                break;
+            case 'Simbolos':
+                simbolosList.add(value);
+                break;
+            case 'UNKNOWN':
+                unknownList.add(value);
+        }
+    }
 }
 
 
 const validateBtn = document.getElementById('analysisBtn');
-    validateBtn.addEventListener('click', ()=> {
-    code = document.getElementById('code').value;    
-    const lexer = new Lexer(code);
+validateBtn.addEventListener('click', () => {
+    palabrasReservadasList.clear();
+    identificadoresList.clear();
+    operadorList.clear();
+    parentesisList.clear();
+    llavesList.clear();
+    simbolosList.clear();
+    editor = document.getElementById('editor').value;
+    const lexer = new Lexer(editor);
     let token;
     let output = document.getElementById('output');
-    output.innerHTML = ''; 
-    while (token = lexer.nextToken()) {
-        output.innerHTML += `
-        <tr">  
-            <td class="py-4 pl-4 sm:pl-6 pr-3 text-sm text-[#dfe6e9]"> ${token.type}:</td>
-            <td class="pr-4 pl-3 text-sm text-[#dfe6e9]">${token.value}</td>
-        </tr>`; 
-    }
+    output.innerHTML = '';
+
+    while (token = lexer.nextToken()) { }
+
+    output.innerHTML += `
+    ${palabrasReservadasList.size > 0 ?
+        `<tr>  
+            <td class="py-4 pl-4 sm:pl-6 pr-3 text-sm text-[#dfe6e9]"> Palabras Reservadas:</td>
+            <td class="pr-4 pl-3 text-sm text-[#dfe6e9]">${Array.from(palabrasReservadasList).join(' , ')}</td>
+        </tr>` : ''}
+    ${identificadoresList.size > 0 ?
+        `<tr>  
+            <td class="py-4 pl-4 sm:pl-6 pr-3 text-sm text-[#dfe6e9]"> Identificadores:</td>
+            <td class="pr-4 pl-3 text-sm text-[#dfe6e9]">${Array.from(identificadoresList).join(' , ')}</td>
+        </tr>` : ''}
+    ${operadorList.size > 0 ?
+        `<tr>  
+            <td class="py-4 pl-4 sm:pl-6 pr-3 text-sm text-[#dfe6e9]"> Operadores:</td>
+            <td class="pr-4 pl-3 text-sm text-[#dfe6e9]">${Array.from(operadorList).join(' , ')}</td>
+        </tr>` : ''}
+    ${parentesisList.size > 0 ?
+        `<tr>  
+            <td class="py-4 pl-4 sm:pl-6 pr-3 text-sm text-[#dfe6e9]"> Paréntesis:</td>
+            <td class="pr-4 pl-3 text-sm text-[#dfe6e9]">${Array.from(parentesisList).join(' , ')}</td>
+        </tr>` : ''}
+    ${llavesList.size > 0 ?
+        `<tr>  
+            <td class="py-4 pl-4 sm:pl-6 pr-3 text-sm text-[#dfe6e9]"> Llaves:</td>
+            <td class="pr-4 pl-3 text-sm text-[#dfe6e9]">${Array.from(llavesList).join(' , ')}</td>
+        </tr>` : ''}
+    ${simbolosList.size > 0 ?
+        `<tr>  
+            <td class="py-4 pl-4 sm:pl-6 pr-3 text-sm text-[#dfe6e9]"> Símbolos:</td>
+            <td class="pr-4 pl-3 text-sm text-[#dfe6e9]">${Array.from(simbolosList).join(' , ')}</td>
+        </tr>` : ''}
+    ${unknownList.size > 0 ?
+        `<tr>  
+            <td class="py-4 pl-4 sm:pl-6 pr-3 text-sm text-[#dfe6e9]"> Desconocidos:</td>
+            <td class="pr-4 pl-3 text-sm text-[#dfe6e9]">${Array.from(unknownList).join(' , ')}</td>
+        </tr>` : ''}
+    `;
 });
 
 
+
 const cleanBtn = document.getElementById('cleanBtn');
-    cleanBtn.addEventListener('click', ()=>{
-        let output = document.getElementById('output');
-        let code = document.getElementById('code');
-        output.innerHTML = ''; 
-        code.innerHTML = ''; 
+cleanBtn.addEventListener('click', () => {
+    let output = document.getElementById('output');
+    output.innerHTML = '';
+    palabrasReservadasList.clear();
+    identificadoresList.clear();
+    operadorList.clear();
+    parentesisList.clear();
+    llavesList.clear();
+    simbolosList.clear();
+
+
 })
 
 const exampleBtn = document.getElementById('exampleBtn');
-var examples = [
-    'fn setPassowrd(str pwd): void{}',
-    'if 12 == age {}',
-    'if algo != algo {}',
-    'for(i = 0, 1 < 5, i ++){}',
-    'str name: "pepe";',
-    'num age: 30;',
-    'bool married: true;'
-];
-    exampleBtn.addEventListener('click', ()=>{
-        let code = document.getElementById('code');
-        var randomExample = examples[Math.floor(Math.random() * examples.length)];
-        code.innerHTML = randomExample; 
-    })  
+let output = document.getElementById('output');
+output.innerHTML = '';
+exampleBtn.addEventListener('click', () => {
+    var examples = [
+        'fn setPassowrd(str pwd): void{}',
+        'if 12 == age {}',
+        'if algo != algo {}',
+        'for(i = 0, 1 < 5, i ++){}',
+        'str name: "pepe";',
+        'num age: 30;',
+        'bool married: true;'
+    ];
+    let editor = document.getElementById('editor');
+    var randomExample = examples[Math.floor(Math.random() * examples.length)];
+    editor.innerHTML = randomExample;
+})  
